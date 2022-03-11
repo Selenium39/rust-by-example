375661d1022643100766e8232e0ae6de85b17137
selenium39 <895484122@qq.com>
2022-03-11 17:33:13 +0800

    delete unuse

--- primitives/target/rls/debug/deps/primitives-442df6c0e39babcf.d
+++
@@ -1,5 +0,0 @@
-/home/selenium39/code-server/rust/rust-by-example/primitives/target/rls/debug/deps/primitives-442df6c0e39babcf.rmeta: src/main.rs
-
-/home/selenium39/code-server/rust/rust-by-example/primitives/target/rls/debug/deps/primitives-442df6c0e39babcf.d: src/main.rs
-
-src/main.rs:
--- primitives/target/rls/debug/deps/primitives-f2bd704e41c98247.d
+++
@@ -1,5 +0,0 @@
-/home/selenium39/code-server/rust/rust-by-example/primitives/target/rls/debug/deps/primitives-f2bd704e41c98247.rmeta: src/main.rs
-
-/home/selenium39/code-server/rust/rust-by-example/primitives/target/rls/debug/deps/primitives-f2bd704e41c98247.d: src/main.rs
-
-src/main.rs:
