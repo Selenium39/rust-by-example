37f019e3add2dfc7ba3bb17a5e1786289eee208e
selenium39 <895484122@qq.com>
2022-03-29 20:03:12 +0800

    scoping-rules

---
+++ scoping-rules/src/main.rs
@@ -0,0 +1,71 @@
+struct MyDrop;
+
+struct Person {
+    name: String,
+    age: u8,
+}
+
+#[derive(Debug)]
+struct Book {
+    // &'static 分配在只读内存区的引用
+    author: &'static str,
+    title: &'static str,
+    year: u32,
+}
+
+fn create_box() {
+    let box1 = Box::new(3i32);
+}
+
+impl Drop for MyDrop {
+    fn drop(&mut self) {
+        println!("析构函数被调用")
+    }
+}
+
+fn changeBook(book: &mut Book) {
+    book.year = 11
+}
+
+fn readBook(book: &Book) {
+    println!("{:?}", book)
+}
+
+fn main() {
+    for _ in 0..100 {
+        // 不需要手动释放内存 RAII
+        create_box()
+    }
+    let my_drop = MyDrop;
+
+    let person = Person {
+        name: String::from("Selenium39"),
+        age: 24,
+    };
+    // name的所有权被转移，但是age的所有权未转移
+    let Person { name, ref age } = person;
+    println!("age:{}", person.age);
+    // 因为name的所有权被转移，所以无法被使用
+    // println!("name:{}", person.name);
+
+    // 不可变引用
+    let book = Book {
+        title: "rust中文",
+        author: "selenium39",
+        year: 10,
+    };
+
+    // 可变引用
+    let mut book1 = book;
+
+    // 不可变的借用不可变对象
+    // changeBook(&book);
+    // readBook(&book);
+    // 可变的借用可变对象
+    changeBook(&mut book1);
+
+    let c = 'C';
+    // 左边的ref等价于右边的&
+    let ref_c1 = &c;
+    let ref ref_c2 = c;
+}
